e4157beb96e90cc389569bc004cdc0d52fad5329
Derek Willis <dwillis@Derek-Willis-Laptop.home>
2008-09-10 09:38:02 -0400

    uncommented admin.autodiscover

--- urls.py
+++ urls.py
@@ -4,7 +4,7 @@ from django.views.generic import list_detail, date_based, create_update
 #from fumblerooski.recruits import views as recruit_views
 from fumblerooski.college import views as college_views
 
-#admin.autodiscover()
+admin.autodiscover()
 
 urlpatterns = patterns('',
     # Example:
@@ -13,13 +13,6 @@ urlpatterns = patterns('',
     # Uncomment this for admin:
      (r'^admin/(.*)', admin.site.root),
      (r'^$', college_views.homepage),
-#     (r'^recruits/positions/$', recruit_views.position_index),
-#     (r'^recruits/positions/(?P<pos>[a-z][a-z]?[a-z]?)/$', recruit_views.position_detail),
-#     (r'^recruits/positions/(?P<pos>[a-z][a-z]?[a-z]?)/(?P<year>all)/$', recruit_views.position_detail),
-#     (r'^recruits/positions/(?P<pos>[a-z][a-z][a-z]?)/(?P<year>2\d\d\d)/$', recruit_views.position_detail),
-#     (r'^recruits/positions/(?P<pos>\w{2,3})/height/(?P<height>\d-\d\d?)/$', recruit_views.position_height),
-#     (r'^recruits/positions/(?P<pos>\w{2,3})/height/under-six-foot/$', recruit_views.position_height_under_six),
-#     (r'^players/(?P<slug>[_a-z0-9]+)/$', recruit_views.player_detail),
      (r'^college/conferences/$', college_views.conference_index),
      (r'^college/conferences/(?P<conf>[-a-z0-9]+)/$', college_views.conference_detail),
      (r'^college/games/$', college_views.game_index),
@@ -43,9 +36,4 @@ urlpatterns = patterns('',
 #    (r'^college/teams/(?P<team>[-a-z]+)/recruits/(?P<pos>[a-z][a-z]?[a-z]?)/$', recruit_views.team_detail_position),
      (r'^states/$', college_views.state_index),
      (r'^states/(?P<state>[a-z][a-z])/$', college_views.state_detail),
-#    (r'^states/(?P<state>[a-z][a-z])/recruits/$', recruit_views.state_detail),
-#     (r'^states/(?P<state>[a-z][a-z])/(?P<pos>[a-z][a-z]?[a-z]?)/recruits/$', recruit_views.state_position_index),
-#     (r'^states/(?P<state>[a-z][a-z])/(?P<city>[-a-z]+)/recruits/$', recruit_views.city_index),
-#     (r'^states/(?P<state>[a-z][a-z])/(?P<city>[-a-z]+)/(?P<school>[-a-z]+)/recruits/$', recruit_views.school_detail),
-#     (r'^states/(?P<state>[a-z][a-z])/(?P<city>[-a-z]+)/(?P<school>[-a-z]+)/(?P<pos>[a-z][a-z]?[a-z]?)/recruits/$', recruit_views.school_detail),
 )
