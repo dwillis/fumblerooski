cbe29b0ee7a98548e323c49beb21da38883f85d3
Derek Willis <dwillis@pc298081585896vivolo.nymsdc.com>
2008-10-06 17:59:55 -0400

    testing drive loader

--- utils.py
+++ utils.py
@@ -400,7 +400,7 @@ def game_drive_loader(game):
         yards = int(cells[10].contents[0])
         time_of_possession = datetime.time(0, int(cells[11].contents[0].split(":")[0]), int(cells[11].contents[0].split(":")[1]))
         print drive, team, quarter, start_how, start_time, start_position, start_side, end_result, end_time, end_position, end_side, plays, yards, time_of_possession
-        d, created = GameDrive.objects.get_or_create(game=game, drive=drive, team=team, quarter=quarter,start_how=start_how, start_time=start_time, start_position=start_position, end_result=end_result, end_time=end_time, end_position=end_position, plays=plays, yards=yards,time_of_possession=time_of_possession)
+        d, created = GameDrive.objects.get_or_create(game=game, drive=drive, team=team, quarter=quarter,start_how=str(start_how), start_time=start_time, start_position=start_position, end_result=str(end_result), end_time=end_time, end_position=end_position, plays=plays, yards=yards,time_of_possession=time_of_possession)
 
 def game_fetcher(year):
     l = get_summary_links(year)
