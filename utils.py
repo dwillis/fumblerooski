c8b0cf2c160d4e1c4076097f2155339d2d229604
Derek Willis <dwillis@d6vdfbf1.nymsdc.com>
2008-10-07 13:11:34 -0400

    added drive team slug

--- utils.py
+++ utils.py
@@ -384,7 +384,7 @@ def game_drive_loader(game):
         try:
             team = College.objects.get(slug=cells[2].contents[0].lower())
         except:
-            team = College.objects.get(drive_slug=cells[2].contents[0])
+            team = College.objects.get(drive_slug=str(cells[2].contents[0]))
         quarter = int(cells[1].contents[0])
         start_how = cells[3].contents[0]
         start_time = datetime.time(0, int(cells[4].contents[0].split(":")[0]), int(cells[4].contents[0].split(":")[1]))
