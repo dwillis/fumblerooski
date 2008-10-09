7336cb5a91935d800e892ae8a07782e55e641d03
Derek Willis <dwillis@Derek-Willis-Laptop.home>
2008-10-08 21:38:04 -0400

    added intcomma and fixes to drive loader

--- utils.py
+++ utils.py
@@ -399,7 +399,7 @@ def game_drive_loader(game):
                 start_side = 'O'
         end_result = DriveOutcome.objects.get(abbrev=str(cells[6].contents[0]))
         end_time = datetime.time(0, int(cells[7].contents[0].split(":")[0]), int(cells[7].contents[0].split(":")[1]))
-        if cells[8].contents and str(cells[8].contents) != 'null':
+        if cells[8].contents and str(cells[8].contents[0]) != 'null':
             try:
                 end_position = int(cells[8].contents[0])
                 end_side = "O"
@@ -413,9 +413,12 @@ def game_drive_loader(game):
         yards = int(cells[10].contents[0])
         time_of_possession = datetime.time(0, int(cells[11].contents[0].split(":")[0]), int(cells[11].contents[0].split(":")[1]))
         print drive, team, quarter, start_how, start_time, start_position, start_side, end_result.abbrev, end_time, end_position, end_side, plays, yards, time_of_possession
-        d, created = GameDrive.objects.get_or_create(game=game, drive=drive, team=team, quarter=quarter,start_how=str(start_how), start_time=start_time, start_position=start_position, start_side=start_side, end_result=end_result, end_time=end_time, end_position=end_position, end_side=end_side, plays=plays, yards=yards,time_of_possession=time_of_possession)
-        if created:
-            print "saved drive %s" % d.drive
+        try:
+            d, created = GameDrive.objects.get_or_create(game=game, drive=drive, team=team, quarter=quarter,start_how=str(start_how), start_time=start_time, start_position=start_position, start_side=start_side, end_result=end_result, end_time=end_time, end_position=end_position, end_side=end_side, plays=plays, yards=yards,time_of_possession=time_of_possession)
+            if created:
+                print "saved drive %s" % d.drive
+        except:
+            pass
 
 def game_fetcher(year):
     l = get_summary_links(year)
