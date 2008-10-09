69279d85c4d326d1945ba21ca815372e543286a6
Derek Willis <dwillis@Derek-Willis-Laptop.home>
2008-10-08 20:25:39 -0400

    fix for null drive end

--- utils.py
+++ utils.py
@@ -399,7 +399,10 @@ def game_drive_loader(game):
                 start_side = 'O'
         end_result = DriveOutcome.objects.get(abbrev=str(cells[6].contents[0]))
         end_time = datetime.time(0, int(cells[7].contents[0].split(":")[0]), int(cells[7].contents[0].split(":")[1]))
-        if cells[8].contents:
+        if cells[8].contents == 'null':
+            end_position = None
+            end_side = 'P'
+        elif cell[8].contents:
             try:
                 end_position = int(cells[8].contents[0])
                 end_side = "O"
