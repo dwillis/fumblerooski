7a7810fce059bb3bf1e277a5851c518f5b006241
Derek Willis <dwillis@cpe-68-174-107-23.nyc.res.rr.com>
2008-10-20 23:43:27 -0400

    fix for player stats loader

--- utils.py
+++ utils.py
@@ -466,7 +466,7 @@ def player_game_stats(game):
             uniform = str(p.find("uniform").contents[0])
             name = str(p.find("name").contents[0])
             try:
-                player = Player.objects.get(team=team, year=year, name=name, number=uniform)
+                player = Player.objects.get(team=team, year=game.date.year, name=name, number=uniform)
                 pg, created = PlayerGame.objects.get_or_create(player=player, game=game, played=True)
                 if p.find("tackles"):
                     un_t = int(p.find("tackles").find("uatackles").contents[0])
@@ -546,7 +546,7 @@ def player_game_stats(game):
                     ps, created = PlayerScoring.objects.get_or_create(player=player, game=game, td=s_td, fg_att=fg_att, fg_made=fg_made, pat_att=pat_att, pat_made=pat_made, two_pt_att=tpt_att, two_pt_made=tpt_made,def_pat_att=d_pat_att, def_pat_made=d_pat_made, def_two_pt_att=d_tpt_att, def_two_pt_made=d_tpt_made, safeties=saf, points=pts)
             except:
                 print "Could not find player: %s (%s)" % (name, uniform)
-                pass
+                raise
             game.has_player_stats = True
             game.save()
 
