8a60b3597c03bc66ed2489a8bda6fb1e045c0ced
Derek Willis <dwillis@cpe-68-174-107-23.nyc.res.rr.com>
2008-10-21 22:16:34 -0400

    adding models to admin

--- college/admin.py
+++ college/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from fumblerooski.college.models import State, College, Game, Coach, Position, Player, PlayerGame, PlayerRush, PlayerPass,PlayerReceiving, PlayerFumble, PlayerScoring, PlayerTackle, PlayerTacklesLoss, PlayerPassDefense, PlayerReturn, CollegeYear, Conference, GameOffense, GameDefense, Week, GameDrive, DriveOutcome, Ranking, RankingType
+from fumblerooski.college.models import State, College, Game, Coach, CoachingJob, CollegeCoach, Position, Player, PlayerGame, PlayerRush, PlayerPass,PlayerReceiving, PlayerFumble, PlayerScoring, PlayerTackle, PlayerTacklesLoss, PlayerPassDefense, PlayerReturn, CollegeYear, Conference, GameOffense, GameDefense, Week, GameDrive, DriveOutcome, Ranking, RankingType
 
 class CollegeAdmin(admin.ModelAdmin):
     list_display = ('name', 'state')
