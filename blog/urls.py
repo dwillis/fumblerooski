cc0d3f823d6febd6ddb7894885efeb6d5ff22815
Derek Willis <dwillis@henddc.com>
2008-11-21 15:21:39 -0500

    fixing url paths

--- blog/urls.py
+++ blog/urls.py
@@ -12,18 +12,18 @@ date_based_dict = {
 }
  
 urlpatterns = patterns("",
-    url(r"^blog/feeds/(?P<url>.*)/$", "django.contrib.syndication.views.feed", {
+    url(r"^feeds/(?P<url>.*)/$", "django.contrib.syndication.views.feed", {
         "feed_dict": feeds,
     }, name="blog_feeds"),
     
-    url(r"^blog/(?P<year>\d{4})/(?P<month>[a-z]{3})/(?P<day>\w{1,2})/(?P<slug>[-\w]+)/$",
+    url(r"^(?P<year>\d{4})/(?P<month>[a-z]{3})/(?P<day>\w{1,2})/(?P<slug>[-\w]+)/$",
         object_detail, dict(date_based_dict, **{
             "template_object_name": "post",
         }), name="blog_post_detail"),
-    url(r"^blog/(?P<year>\d{4})/(?P<month>[a-z]{3})/(?P<day>\w{1,2})/$",
+    url(r"^(?P<year>\d{4})/(?P<month>[a-z]{3})/(?P<day>\w{1,2})/$",
         archive_day, date_based_dict, name="blog_archive_daily"),
-    url(r"^blog/(?P<year>\d{4})/(?P<month>[a-z]{3})/$",
+    url(r"^(?P<year>\d{4})/(?P<month>[a-z]{3})/$",
         archive_month, date_based_dict, name="blog_archive_month"),
-    url(r"^blog/(?P<year>\d{4})/$",
+    url(r"^(?P<year>\d{4})/$",
         archive_year, date_based_dict, name="blog_archive_year"),
 )
