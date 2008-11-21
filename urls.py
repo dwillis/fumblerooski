b0ca4d2ff01abc52bbac9cc49e8f37695745c52c
Derek Willis <dwillis@henddc.com>
2008-11-21 13:37:52 -0500

    fixed main urls

--- urls.py
+++ urls.py
@@ -7,5 +7,5 @@ urlpatterns = patterns('',
      url(r"^admin/(.*)", admin.site.root),
      url(r"^blog/", include("blog.urls")),
      url(r"^college/", include("college.urls")),
-     url(r"^$", "college.views.homepage"),
+     url(r"^$", "fumblerooski.college.views.homepage"),
 )
