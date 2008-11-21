2e283fae1fc541522c1e1f3d076c078cee7e51f4
Derek Willis <dwillis@henddc.com>
2008-11-21 14:01:30 -0500

    adding admindocs

--- urls.py
+++ urls.py
@@ -4,6 +4,7 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
+     url(r'^admin/doc/', include('django.contrib.admindocs.urls')), 
      url(r"^admin/(.*)", admin.site.root),
      url(r"^blog/", include("fumblerooski.blog.urls")),
      url(r"^college/", include("fumblerooski.college.urls")),
