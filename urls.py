0356d8331828f5d319223655f0bff2693433eb56
Derek Willis <dwillis@henddc.com>
2008-11-21 13:39:03 -0500

    fixed main urls

--- urls.py
+++ urls.py
@@ -5,7 +5,7 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
      url(r"^admin/(.*)", admin.site.root),
-     url(r"^blog/", include("blog.urls")),
-     url(r"^college/", include("college.urls")),
+     url(r"^blog/", include("fumblerooski.blog.urls")),
+     url(r"^college/", include("fumblerooski.college.urls")),
      url(r"^$", "fumblerooski.college.views.homepage"),
 )
