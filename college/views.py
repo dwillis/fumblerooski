91fba5ea40af82e75a9dc80229d35a8f27f7e260
Derek Willis <dwillis@Derek-Willis-Laptop.home>
2008-11-27 22:14:19 -0500

    fixing rankings view

--- college/views.py
+++ college/views.py
@@ -84,7 +84,7 @@ def team_rankings_season(request, team, season):
     cy = get_object_or_404(CollegeYear, college__slug=team, year=season)
     date = datetime.date.today()-datetime.timedelta(days=7)
     latest_week = Week.objects.get(year=season, end_date__gte=date, end_date__lte=datetime.date.today())
-    latest_rankings = Ranking.objects.filter(college=cy.college, year=year, week=latest_week).select_related().order_by('-college_week.week_num')
+    latest_rankings = Ranking.objects.filter(college=cy.college, year=season, week=latest_week).select_related().order_by('-college_week.week_num')
     return render_to_response('college/team_rankings_season.html', {'season_record': cy, 'latest_rankings': latest_rankings})
 
 def team_opponents(request, team):
