5ed431b30804d6b8dcaaaa88d4a3142757474958
Derek Willis <dwillis@Derek-Willis-Laptop.home>
2008-11-29 09:52:21 -0500

    fixed bug in latest week for rankings

--- college/views.py
+++ college/views.py
@@ -30,7 +30,7 @@ def rankings_index(request):
 def rankings_season(request, rankingtype, season, div='B'):
     rt = get_object_or_404(RankingType, slug=rankingtype)
     date = datetime.date.today()-datetime.timedelta(days=7)
-    latest_week = Week.objects.get(year=season, end_date__gte=date, end_date__lte=datetime.date.today())
+    latest_week = Week.objects.filter(year=season, end_date__gte=date, end_date__lte=datetime.date.today()).order_by('end_date')[0]
     rankings_list = Ranking.objects.filter(year=season, ranking_type=rt, week=latest_week, division=div).select_related().order_by('rank')
     return render_to_response('college/rankings_season.html', {'ranking_type': rt, 'rankings_list': rankings_list, 'season':season, 'latest_week':latest_week})
 
