a3ba7c1a045f75b9400051fa5008f0fc736aec93
Derek Willis <dwillis@Derek-Willis-Laptop.local>
2008-12-26 15:50:59 -0500

    added other weeks to team rankings

--- college/views.py
+++ college/views.py
@@ -103,6 +103,7 @@ def team_rankings_season(request, team, season):
     cy = get_object_or_404(CollegeYear, college__slug=team, year=season)
     date = datetime.date.today()-datetime.timedelta(days=7)
     latest_week = Week.objects.get(year=season, end_date__gte=date, end_date__lte=datetime.date.today())
+    other_weeks = Week.objects.filter(year=season).exclude(week_num=latest_week.week_num).exclude(end_date__gte=datetime.date.today()).order_by('end_date')
     latest_rankings = Ranking.objects.filter(college=cy.college, year=season, week=latest_week).select_related().order_by('-college_week.week_num')
     return render_to_response('college/team_rankings_season.html', {'season_record': cy, 'latest_rankings': latest_rankings, 'latest_week': latest_week})
 
