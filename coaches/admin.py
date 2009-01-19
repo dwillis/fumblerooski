4f34573778ba207df28cc576a01467964c4c5b85
Derek Willis <dwillis@Derek-Willis-Laptop.home>
2009-01-19 15:50:35 -0500

    cleaning up name fields

--- coaches/admin.py
+++ coaches/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from fumblerooski.coaches.models import Coach, CoachingJob
 
 class CoachAdmin(admin.ModelAdmin):
-    prepopulated_fields = {'slug': ('name',)}
+    prepopulated_fields = {'slug': ('first_name','last_name')}
 
 class CoachingJobAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('name',)}
