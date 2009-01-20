4e287d15ea474e910f8e3745a93e6194c106ebe6
Derek Willis <dwillis@Derek-Willis-Laptop.home>
2009-01-19 21:29:26 -0500

    fixing coaching views

--- coaches/views.py
+++ coaches/views.py
@@ -8,13 +8,13 @@ CURRENT_SEASON = 2009
 
 def coach_index(request):
     two_months_ago = datetime.date.today()-datetime.timedelta(60)
-    active_hc = CollegeCoach.objects.filter(job__name='Head Coach', end_date__isnull=True, collegeyear__year=CURRENT_SEASON).select_related().order_by('start_date')
-    recent_departures = CollegeCoach.objects.filter(job__name='Head Coach', end_date__gte=two_months_ago).select_related().order_by('end_date')[:10] or None
+    active_hc = CollegeCoach.objects.select_related().filter(jobs__name='Head Coach', end_date__isnull=True, collegeyear__year=CURRENT_SEASON).order_by('-start_date')
+    recent_departures = CollegeCoach.objects.select_related().filter(jobs__name='Head Coach', end_date__gte=two_months_ago).order_by('end_date')[:10]
     return render_to_response('coaches/coach_index.html', {'active_coaches': active_hc, 'recent_departures': recent_departures })
 
 def coach_detail(request, coach):
     c = get_object_or_404(Coach, slug=coach)
-    college_list = CollegeCoach.objects.filter(coach=c).order_by('-start_year')
+    college_list = CollegeCoach.objects.filter(coach=c).select_related().order_by('-college_collegeyear.year')
     if college_list[0].end_date == None and college_list[0].end_year == None:
         current_job = college_list[0]
         college_list = college_list[1:]
