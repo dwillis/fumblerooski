37578f77ee0eea174c9e2832df838ea082802025
Derek Willis <dwillis@gmail.com>
2009-06-17 20:47:44 -0400

    fixed coaches urls

--- urls.py
+++ urls.py
@@ -7,7 +7,7 @@ urlpatterns = patterns('',
      url(r'^admin/doc/', include('django.contrib.admindocs.urls')), 
      url(r"^admin/(.*)", admin.site.root),
      url(r"^blog/", include("fumblerooski.blog.urls")),
-     url(r"^coaches", include("fumblerooski.coaches.urls")),
+     url(r"^coaches/", include("fumblerooski.coaches.urls")),
      url(r"^college/", include("fumblerooski.college.urls")),
      url(r"^$", "fumblerooski.college.views.homepage"),
 )
