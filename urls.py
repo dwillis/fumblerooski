a2377ba89cd9937e2c8b2acb96b202983983c50f
Derek Willis <dwillis@gmail.com>
2009-08-16 22:06:15 -0400

    fixed coach urls

--- urls.py
+++ urls.py
@@ -7,7 +7,14 @@ urlpatterns = patterns('',
      url(r'^admin/doc/', include('django.contrib.admindocs.urls')), 
      url(r"^admin/(.*)", admin.site.root),
      url(r"^blog/", include("fumblerooski.blog.urls")),
-     url(r"^coaches/", include("fumblerooski.college.urls")),
      url(r"^college/", include("fumblerooski.college.urls")),
      url(r"^$", "fumblerooski.college.views.homepage"),
+)
+
+urlpatterns += patterns('fumblerooski.college.views',
+     url(r'^coaches/$', 'coach_index'),
+     url(r'^coaches/active/$', 'active_coaches'),
+     url(r'^coaches/feeds/recent_hires/$', 'recent_hires_feed'),
+     url(r'^coaches/detail/(?P<coach>[-a-z]+)/$', 'coach_detail'),
+     url(r'^coaches/assistants/$', 'assistant_index'),
 )
