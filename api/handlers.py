dd50ed0e8b065f3e515c7ce00d05ffe6eb81c129
Derek Willis <dwillis@new-host.home>
2009-11-21 09:21:34 -0500

    fixed team api url

--- api/handlers.py
+++ api/handlers.py
@@ -1,6 +1,6 @@
 from piston.handler import BaseHandler
 from piston.utils import rc
-from fumblerooski.college.models import College
+from fumblerooski.college.models import College, Coach
 
 class CollegeHandler(BaseHandler):
     allowed_methods = ('GET',)
@@ -17,6 +17,25 @@ class CollegeHandler(BaseHandler):
             return rc.BAD_REQUEST
         
     
+    @classmethod
+    def resource_uri(cls, college):
+        return ('colleges', ['json',])
+
+class CoachHandler(BaseHandler):
+    allowed_methods = ('GET',)
+    model = Coach
+    fields = ('id', 'updated', 'name', 'slug', ('state', ('id', 'name')), ('collegeyear_set', ('year','record','conference_record', 'division', ('conference', ('abbrev','name')))))
+    exclude = ('official_url','official_rss', 'drive_slug')
+
+    def read(self, request, slug):
+        try:
+            return Coach.objects.get(slug=slug)
+        except Coach.DoesNotExist:
+            return rc.NOT_FOUND
+        except Coach.MultipleObjectsReturned:
+            return rc.BAD_REQUEST
+
+
     @classmethod
     def resource_uri(cls, college):
         return ('colleges', ['json',])
--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls.defaults import *
 from piston.resource import Resource
-from fumblerooski.api.handlers import CollegeHandler
+from fumblerooski.api.handlers import CollegeHandler, CoachHandler
 
 college_handler = Resource(CollegeHandler)
+coach_handler = Resource(CoachHandler)
 
 urlpatterns = patterns('',
-   url(r'^college/(?P<slug>[^/]+)/', college_handler),
+   url(r'^college/teams/(?P<slug>[^/]+)/$', college_handler),
+   url(r'^coach/(?P<slug>[^/]+)/$', coach_handler),
 )
