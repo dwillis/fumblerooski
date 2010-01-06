7dc64ee4389d5f499a84e8d17c503986f302304a
Derek Willis <derek@new-host.home>
2010-01-05 22:01:52 -0500

    fixed api urls

--- api/urls.py
+++ api/urls.py
@@ -1,12 +1,13 @@
 from django.conf.urls.defaults import *
 from piston.resource import Resource
-from fumblerooski.api.handlers import CollegeHandler, CoachHandler
+from fumblerooski.api.handlers import CollegeHandler, CoachHandler, CollegeYearHandler
 
 college_handler = Resource(CollegeHandler)
 coach_handler = Resource(CoachHandler)
+college_year_handler = Resource(CollegeYearHandler)
 
 urlpatterns = patterns('',
    url(r'^college/teams/(?P<slug>[^/]+)/$', college_handler),
-   url(r'^college/teams/(?P<slug>[^/]+)/(?P<year>\d\d\d\d)/$', collegeyear_handler),
+   url(r'^college/teams/(?P<slug>[^/]+)/(?P<year>\d\d\d\d)/$', college_year_handler),
    url(r'^coach/(?P<slug>[^/]+)/$', coach_handler),
 )
